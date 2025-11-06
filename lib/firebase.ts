70c8b272ea6a12ebcb41faaea2ca9212a251f029
PhuPham <tranphupham2909@gmail.com>
2025-11-07 02:48:43 +0700

    [add] envi

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvyvGPdqgT7ZWFJCqQ-ErF0jOdBMYQYu8",
-  authDomain: "tracuudulieu-bfa8c.firebaseapp.com",
-  projectId: "tracuudulieu-bfa8c",
-  storageBucket: "tracuudulieu-bfa8c.firebasestorage.app",
-  messagingSenderId: "798088355384",
-  appId: "1:798088355384:web:f8c159f74cb5d0e11850cd",
-  measurementId: "G-KRJS161LN4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
